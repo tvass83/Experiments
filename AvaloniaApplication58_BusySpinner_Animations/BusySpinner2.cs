01449029c13ebaf33df7c16493fed11b995aa76d
Tamas Vass <tamas.vass@synergysports.com>
2022-11-01 21:56:10 +0100

    AvaloniaApplication58_BusySpinner_Animations

---
+++ AvaloniaApplication58_BusySpinner_Animations/BusySpinner2.cs
@@ -0,0 +1,51 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using Avalonia.Threading;
+using System;
+
+namespace Synergy.Controls
+{
+    public sealed class BusySpinner2 : ContentControl
+    {
+        private DispatcherTimer _animationTimer;
+        private RotateTransform _rotateTransform;
+
+        static BusySpinner2()
+        {
+            IsVisibleProperty.OverrideDefaultValue<BusySpinner2>(false);
+        }
+
+        public bool EnableStepMode { get; set; }
+
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+
+            if (EnableStepMode)
+            {
+                var _viewbox = e.NameScope.Find("viewbox") as Visual;
+                _rotateTransform = _viewbox.RenderTransform as RotateTransform;
+
+                _animationTimer = new DispatcherTimer();
+                _animationTimer.Interval = TimeSpan.FromMilliseconds(75);
+                _animationTimer.Tick += OnAnimationTimerTick;
+
+                this.GetObservable(IsVisibleProperty)
+                    .Subscribe(isVisible =>
+                    {
+                        if (isVisible)
+                            _animationTimer.Start();
+                        else
+                            _animationTimer.Stop();
+                    });
+            }
+        }
+
+        private void OnAnimationTimerTick(object? sender, EventArgs e)
+        {
+            _rotateTransform.Angle = (_rotateTransform.Angle + 36) % 360;
+        }
+    }
+}
---
+++ AvaloniaApplication58_BusySpinner_Animations/DiscreteDoubleAnimator.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Animation.Animators;
+using Avalonia.Media;
+using System;
+using System.Diagnostics;
+
+namespace Synergy.Controls
+{
+    /// <summary>
+    /// Avalonia 0.10.7
+    /// https://github.com/AvaloniaUI/Avalonia/pull/6082
+    /// </summary>
+    public class DiscreteDoubleAnimator : Animator<double>
+    {
+        public override double Interpolate(double progress, double oldValue, double newValue)
+        {
+            int step = 36;
+            var length = ((int)(progress * newValue)) / step;
+
+            Debug.WriteLine($"TV: {length * step}");
+
+            return length * step;
+        }
+
+        public override IDisposable Apply(Animation animation, Animatable control, IClock clock, IObservable<bool> obsMatch, Action onComplete)
+        {
+            Visual ctrl = (Visual)control;
+
+            if (ctrl.RenderTransform == null)
+            {
+                ctrl.RenderTransform = new RotateTransform();
+            }
+
+            return base.Apply(animation, (Transform)ctrl.RenderTransform, clock, obsMatch, onComplete);
+        }
+    }
+}
