8349b3498ff82caaa9fb4267c22fa4dd3a645eca
Tamas Vass <t.vass.ext@sportradar.com>
2024-04-04 17:11:21 +0200

    AvaloniaApp67_SideExpanderWithWrapPanel initial commit

---
+++ AvaloniaApp67_SideExpanderWithWrapPanel/Program.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+
+namespace AvaloniaApp67_SideExpanderWithWrapPanel
+{
+    internal class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                //Better to be more specific instead of using UsePlatformDetect()
+                .UseWin32() // or UseAvaloniaNative() or UseX11()
+                .With(new Win32PlatformOptions() { UseWindowsUIComposition = false })
+                .UseDirect2D1() //or UseSkia()
+                .LogToTrace();
+    }
+}
