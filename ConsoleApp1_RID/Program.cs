b0a08332e2f51304563660723a2c1e81d9cca994
Tamas Vass <t.vass.ext@sportradar.com>
2024-10-09 10:10:42 +0200

    Initial version of ConsoleApp1_RID

---
+++ ConsoleApp1_RID/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ConsoleApp1_RID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Window();
+        }
+    }
+}
